fbe97fed48f43e6a150d96afc9b579d984eb89a0
Joelle Cayen <joelle.cayen@canada.ca>
2023-11-17 16:25:48 -0500

    initial structure and js code

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* * * * * * * * * * * * * *
+*           MAIN           *
+* * * * * * * * * * * * * */
+
+// init global variables & switches
+
+// load data using promises
+let promises = [
+    d3.csv("Data/cleaned_data/csv_format_d3/abortion_sentiment.csv"), //0
+    d3.csv("Data/cleaned_data/csv_format_d3/alliances.csv"), //1
+    d3.csv("Data/cleaned_data/csv_format_d3/china_sentiment.csv"), //2
+    d3.csv("Data/cleaned_data/csv_format_d3/climate_sentiment.csv"), //3
+    d3.csv("Data/cleaned_data/csv_format_d3/country_state_mentions.csv"), //4
+    d3.csv("Data/cleaned_data/csv_format_d3/economy.csv"), //5
+    d3.csv("Data/cleaned_data/csv_format_d3/nafta_sentiment.csv"), //6
+    d3.csv("Data/cleaned_data/csv_format_d3/nato_sentiment.csv"), //7
+    d3.csv("Data/cleaned_data/csv_format_d3/oil_sentiment.csv"), //8
+    d3.csv("Data/cleaned_data/csv_format_d3/policy.csv"), //9
+    d3.csv("Data/cleaned_data/csv_format_d3/policy_agg.csv"), //10
+    d3.csv("Data/cleaned_data/csv_format_d3/speech_length.csv"), //11
+    d3.csv("Data/cleaned_data/csv_format_d3/speech_length_agg.csv") //12
+
+    // add more files here
+];
+
+Promise.all(promises)
+    .then(function (data) {
+        initMainPage(data)
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+
+// initMainPage
+function initMainPage(dataArray) {
+
+    // log data
+    console.log('check out the data', dataArray);
+
+    // init visualizations
+    myBarVis = new BarVis('racing-bar-chart', dataArray[12]);
+
+    myStackedBarVis = new StackedBarVis('stacked-bars-themes', dataArray[10]);
+
+
+
+}
---
+++ js/runningBarVis.js
@@ -0,0 +1,146 @@
+/* * * * * * * * * * * * * *
+*      class BarVis        *
+* * * * * * * * * * * * * */
+
+class BarVis {
+
+    constructor(parentElement, data) {
+        this.parentElement = parentElement;
+        this.data = data;
+
+        this.initVis();
+    }
+
+    initVis(){
+        let vis = this;
+
+        vis.margin = {top: 20, right: 20, bottom: 60, left: 120};
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+
+        // init drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append('g')
+            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+
+
+        // Scales and axes
+        vis.y = d3.scaleBand()
+            .range([vis.height, 0])
+            .padding(0.1);
+
+        vis.x = d3.scaleLinear()
+            .range([0, vis.width]);
+
+        vis.yAxis = d3.axisLeft()
+            .scale(vis.y);
+
+        vis.xAxis = d3.axisBottom()
+            .scale(vis.x)
+
+        vis.svg.append("g")
+            .attr("class", "y-axis axis");
+
+        vis.svg.append("g")
+            .attr("class", "x-axis axis")
+            .attr("transform", "translate(0," + (vis.height) + ")");
+
+
+        // legend
+        var keys = ["Republican", "Democratic", "Other"]
+
+        var color = d3.scaleOrdinal()
+            .domain(keys)
+            .range(["#FF8B8B", "#83A2FF", "#FFD28F"]);
+
+        var size=20
+
+        vis.svg.append("g")
+            .attr("class", "legend")
+            .selectAll(".myLegend")
+            .data(keys)
+            .enter()
+            .append("rect")
+            .attr("class", "myLegend")
+            .attr("x", vis.width-100)
+            .attr("y", function(d,i){ return 10 + i*(size+5)})
+            .attr("width", size)
+            .attr("height", size)
+            .style("fill", function(d){ return color(d)})
+
+        vis.svg.append("g")
+            .attr("class", "legend")
+            .selectAll("mylabels")
+            .data(keys)
+            .enter()
+            .append("text")
+            .attr("x", vis.width-100 + size*1.2)
+            .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)})
+            .style("fill", function(d){ return color(d)})
+            .text(function(d){ return d})
+            .attr("text-anchor", "left")
+            .style("alignment-baseline", "middle")
+
+        this.wrangleData();
+
+    }
+
+    wrangleData(){
+        let vis = this;
+
+        vis.displayData = vis.data.sort((a,b) => {return a.year - b.year});
+
+        vis.updateVis();
+
+    }
+
+    updateVis(){
+        let vis = this;
+
+        var t = 800;
+
+        vis.x.domain([0, 20000]) //d3.max(vis.displayData, d => d.word_count)
+        vis.y.domain(vis.displayData.map(d => d.name))
+
+        vis.svg.select(".x-axis")
+            .transition()
+            .duration(t)
+            .call(vis.xAxis);
+
+        vis.svg.select(".y-axis")
+            .transition()
+            .duration(t)
+            .call(vis.yAxis);
+
+        vis.bars = vis.svg.selectAll(".racingBars")
+            .data(vis.displayData)
+
+        vis.bars.enter()
+            .append("rect")
+            .attr("class", "racingBars")
+            .merge(vis.bars)
+            .attr("y", d => vis.y(d.name))
+            .attr("height", vis.y.bandwidth())
+            .attr("x", 0)
+            .transition()
+            .duration(1100)
+            .attr("width", d => vis.x(d.word_count))
+
+            .attr("fill", d=>{if(d.party == "Republican"){
+                return "#FF8B8B"
+            }else if(d.party == "Democratic"){
+                return "#83A2FF"
+            }else{
+                return "#FFD28F"
+            }});
+
+        vis.bars.exit().remove();
+
+
+
+    }
+
+
+}
