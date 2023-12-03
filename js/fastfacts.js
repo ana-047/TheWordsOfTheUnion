f4a78ec565b807d39c5d5df3a30dc822a15a9512
AnaR <anahataraicar@hotmail.com>
2023-12-02 22:31:53 -0500

    flip squares wip

--- js/fastfacts.js
+++ js/fastfacts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const gridContainer = document.getElementById('gridContainer');
 
     const questionsAndAnswers = [
-        { question: "", answer: "Answer 1" },
+        { question: "Question 1", answer: "Answer 1" },
         { question: "Question 2", answer: "Answer 2" },
         { question: "Question 3", answer: "Answer 3" },
         { question: "Question 4", answer: "Answer 4" },
@@ -13,21 +13,37 @@ document.addEventListener('DOMContentLoaded', () => {
         { question: "Question 9", answer: "Answer 9" },
     ];
 
-    questionsAndAnswers.forEach((item, index) => {
-        const card_ff = document.createElement('div');
-        card_ff.classList.add('card_ff');
+    // Define the number of rows and columns in the grid
+    const numRows = 3;
+    const numCols = 3;
 
-        const front = document.createElement('div');
-        front.classList.add('front');
-        front.innerText = item.question;
+    for (let row = 0; row < numRows; row++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('gridRow');
 
-        const back = document.createElement('div');
-        back.classList.add('back');
-        back.innerText = item.answer;
+        for (let col = 0; col < numCols; col++) {
+            const card_ff = document.createElement('div');
+            card_ff.classList.add('card_ff');
 
-        card_ff.appendChild(front);
-        card_ff.appendChild(back);
+            const index = row * numCols + col;
+            const item = questionsAndAnswers[index];
 
-        gridContainer.appendChild(card_ff);
-    });
+            if (item) {
+                const front = document.createElement('div');
+                front.classList.add('front');
+                front.innerText = item.question;
+
+                const back = document.createElement('div');
+                back.classList.add('back');
+                back.innerText = item.answer;
+
+                card_ff.appendChild(front);
+                card_ff.appendChild(back);
+            }
+
+            gridRow.appendChild(card_ff);
+        }
+
+        gridContainer.appendChild(gridRow);
+    }
 });
